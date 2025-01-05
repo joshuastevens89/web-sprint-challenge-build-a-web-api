9018de1a6d2ae75166f3ce13e9098dd3cacb2951
Joshua Stevens <e2855845@student.dcccd.edu>
2025-01-05 16:48:19 -0600

    built the actions-router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,6 +3,21 @@ const express = require('express');
 
 const Actions = require('./actions-model');
 
+const Projects = require('../projects/projects-model');
+
 const router = express.Router();
+
+const validateActionId = async (req, res, next) => {
+    try {
+        const action = await Actions.get(req.params.id);
+        if (!action) {
+            return res.status(404).json({ message: 'Action not found'});
+        }
+        req.action = action;
+        next();
+    } catch (err) {
+        next(err);
+    }
+};
 
 module.exports = router;
