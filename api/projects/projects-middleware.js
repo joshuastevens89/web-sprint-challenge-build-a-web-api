a1bd5241787801a1eafd64eae2b3d5fe23a65e13
Joshua Stevens <e2855845@student.dcccd.edu>
2025-01-05 16:29:55 -0600

    got the server up and running

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,17 @@
-// add middlewares here related to projects
+function logger(req, res, next) {
+    console.log('logger middleware')
+    next()
+}
+
+function validateUserId(req, res, next) {
+    console.log('validateUserId middleware')
+    next()
+}
+
+
+
+module.exports = {
+    logger,
+    validateUserId,
+
+}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const server = express();
 
+
+
+
+
+
+server.use(express.json());
+
+
+
+
+
+
+
+
+
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+
 
 module.exports = server;
